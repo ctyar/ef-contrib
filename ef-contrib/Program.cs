d26678591582f1bdc524a159a5612c8d3f0d9bb8
Shahriar Gholami <shrgholami@gmail.com>
2020-03-23 18:23:18 +0430

    Add project files.

---
+++ ef-contrib/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace Ctyar.Ef.Contrib
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            var rootCommand = new RootCommand();
+
+            var squashCommand = new Command("--squash")
+            {
+                Handler = CommandHandler.Create(Squash)
+            };
+            squashCommand.AddAlias("-s");
+
+            rootCommand.Add(squashCommand);
+            
+            return rootCommand.Invoke(args);
+        }
+
+        private static void Squash()
+        {
+            
+        }
+    }
+}

70e971db949d788e590e88355a0da7c2f30aaec0
Shahriar Gholami <shrgholami@gmail.com>
2020-03-23 19:21:08 +0430

    Add a sqush command with lots of hard coded stuff

--- ef-contrib/Program.cs
+++ ef-contrib/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Ctyar.Ef.Contrib
 {
@@ -9,22 +8,72 @@ namespace Ctyar.Ef.Contrib
     {
         private static int Main(string[] args)
         {
-            var rootCommand = new RootCommand();
+            var rootCommand = new RootCommand
+            {
+                Handler = CommandHandler.Create(Squash)
+            };
 
-            var squashCommand = new Command("--squash")
+            /*var squashCommand = new Command("--squash")
             {
                 Handler = CommandHandler.Create(Squash)
             };
             squashCommand.AddAlias("-s");
 
-            rootCommand.Add(squashCommand);
-            
+            rootCommand.Add(squashCommand);*/
+
             return rootCommand.Invoke(args);
         }
 
         private static void Squash()
         {
-            
+            const string workingDirectory = @"C:\Shahriar\Projects\whynotearth\meredith-core";
+
+            Remove("ShareImage", workingDirectory);
+
+            Remove("LandingPageData", workingDirectory);
+
+            AddMigration("NewMigration", workingDirectory);
+        }
+
+        private static void Remove(string migrationName, string workingDirectory)
+        {
+            UpdateDatabase(migrationName, workingDirectory);
+
+            RemoveLastMigration(workingDirectory);
+        }
+
+        private static void RemoveLastMigration(string workingDirectory)
+        {
+            Execute("ef migrations remove -p WhyNotEarth.Meredith.Data.Entity -s WhyNotEarth.Meredith.App", workingDirectory);
+        }
+
+        private static void AddMigration(string migrationName, string workingDirectory)
+        {
+            Execute($"ef migrations add {migrationName} -p WhyNotEarth.Meredith.Data.Entity -s WhyNotEarth.Meredith.App", workingDirectory);
+        }
+
+        private static void UpdateDatabase(string migrationName, string workingDirectory)
+        {
+            Execute(
+                $"ef database update {migrationName} -p WhyNotEarth.Meredith.Data.Entity -s WhyNotEarth.Meredith.App",
+                workingDirectory);
+        }
+
+        private static void Execute(string command, string workingDirectory)
+        {
+            using var process = new System.Diagnostics.Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = command,
+                    CreateNoWindow = false,
+                    WorkingDirectory = workingDirectory
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
         }
     }
 }
